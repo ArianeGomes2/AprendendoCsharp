0db23343ff38d2002afb6d5e1ad60b0b56632fa5
Leonardo-Nascimento <leonardot.i@hotmail.com>
2020-07-22 19:49:04 -0300

    Resolvendo lista de exercícios 1

---
+++ Lista de Exercícios 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lista_de_Exercícios_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Vou aprender a programar no meu tempo!"); // questão 4
+
+            // questão 5
+          /*  Console.WriteLine("Digite um nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Você digitou o nome " + nome);*/
+
+          /*  // questão 6
+            Console.WriteLine("Digite seu nome:");
+            string nome1 = Console.ReadLine();
+            Console.WriteLine("Digite seu sobrenome:");
+            string sobrenome = Console.ReadLine();
+            Console.WriteLine(nome1);
+            Console.WriteLine(sobrenome);*/
+
+         /*   //questão 9
+            Console.WriteLine("Digite um nome:");
+            string nome2 = Console.ReadLine();
+            Console.WriteLine("Digite um sobrenome:");
+            string sobrenome2 = Console.ReadLine();
+            Console.Write(nome2);
+            Console.Write(sobrenome2);*/
+
+           //questão 10 
+            Console.WriteLine("Digite um nome:");
+            string nome3 = Console.ReadLine();
+            Console.WriteLine("Digite um sobrenome:");
+            string sobrenome3 = Console.ReadLine();
+            Console.WriteLine(nome3 + " " + sobrenome3);
+            
+        }
+    }
+}
---
+++ Lista_Exercícios_01/Lista_Exercícios_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lista_Exercícios_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
