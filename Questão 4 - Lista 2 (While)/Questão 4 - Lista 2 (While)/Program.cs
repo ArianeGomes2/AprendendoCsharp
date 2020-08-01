c534591f1bf8aac37e370dde822b40b8648896ca
Leonardo-Nascimento <leonardot.i@hotmail.com>
2020-08-01 19:44:03 -0300

    Questão 4 da Lista 2 usando While

---
+++ Questão 4 - Lista 2 (While)/Questão 4 - Lista 2 (While)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Questão_4___Lista_2__While_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+
+            while (number != 0)
+            {
+                Console.WriteLine("Digite um valor inteiro:");
+                number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number + " é par!");
+                }
+                else
+                {
+                    Console.WriteLine(number + " é ímpar!");
+                }
+
+            }
+        }
+    }
+}
