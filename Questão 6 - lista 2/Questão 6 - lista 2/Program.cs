948478b7da5522dd943ec78abd8ddb60cf73d182
Leonardo-Nascimento <leonardot.i@hotmail.com>
2020-08-02 11:45:10 -0300

    Questão 5 e 6 - Lista 2

---
+++ Questão 6 - lista 2/Questão 6 - lista 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Questão_6___lista_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100;
+
+            while (number <= 200) {
+     
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine(number);
+                    }
+                number++;
+
+            }
+                
+        }    
+    }
+}
---
+++ Questão 7 - Lista 2/Questão 7 - Lista 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Questão_7___Lista_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100;
+
+            while(number <= 200)
+            {
+                if (number % 2 != 0){
+                    Console.WriteLine(number);
+                }
+              
+                number++;
+            }
+        }
+    }
+}
