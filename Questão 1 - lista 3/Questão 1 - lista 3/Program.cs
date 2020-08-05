f88b67ad33f0d9ce880a9f405d434ca05a07bc7e
Leonardo-Nascimento <leonardot.i@hotmail.com>
2020-08-05 19:48:36 -0300

    Questão 1 da lista 3 e Questão 2 da lista 4

---
+++ Questão 1 - lista 3/Questão 1 - lista 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Questão_1___lista_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a contidade de elementos:");
+            int elements = int.Parse(Console.ReadLine());
+            int[] vet = new int[elements];
+
+            for (int i = 1; i <= elements; i++)
+            {
+                vet[i] = int.Parse(Console.ReadLine());
+            }for (int i = 1; i <= elements; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+        }
+    }
+}
---
+++ Questão 2 -  Lista 4/Questão 2 -  Lista 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Questão_2____Lista_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de elementos:");
+            int n = int.Parse(Console.ReadLine());
+            int[] vet = new int[n];
+            int maior = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Informe o elemento de índice" + "{" + i + "}:");
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Os números digitados foram:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {               
+                if (vet[i] > maior)
+                {
+                    maior = vet[i];
+                }
+            }
+
+            Console.WriteLine("O maior número digitado foi: " + maior);
+
+        }
+    }
+}
